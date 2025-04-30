ad82d8028825b98c1672f5389d78b235f1712e44
Wolfyre243 <wolfyreblueflare@gmail.com>
2025-05-01 03:42:39 +0800

    clearred up rredundant stuff

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
+    // TODO: Hide the port somehow
     baseURL: window.location.protocol + "//" + window.location.hostname + ":" + 5000,
     timeout: 10000,
     headers: {
@@ -8,29 +9,29 @@ const api = axios.create({
     },
 });
 
-// // Request interceptor
-// api.interceptors.request.use(
-//     (config) => {
-//         // Perform actions before sending request
-//         return config;
-//     },
-//     (error) => {
-//         // Handle request errors
-//         return Promise.reject(error);
-//     }
+// Request interceptor
+api.interceptors.request.use(
+    (config) => {
+        // Perform actions before sending request
+        return config;
+    },
+    (error) => {
+        // Handle request errors
+        return Promise.reject(error);
+    }
 
-// );
+);
 
-// // Response interceptor
-// api.interceptors.response.use(
-//     (response) => {
-//       // Modify the response data
-//       return response;
-//     },
-//     (error) => {
-//       // Handle response errors
-//       return Promise.reject(error);
-//     }
-// );
+// Response interceptor
+api.interceptors.response.use(
+    (response) => {
+      // Modify the response data
+      return response;
+    },
+    (error) => {
+      // Handle response errors
+      return Promise.reject(error);
+    }
+);
 
 export default api;
